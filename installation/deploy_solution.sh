0f866364d3ed5b492fb8f7bd12baaa5a3c44f32b
danloZ <danloZ@users.noreply.github.com>
2023-01-13 17:40:51 +0100

    Update deploy_solution.sh

--- installation/deploy_solution.sh
+++ installation/deploy_solution.sh
@@ -5,10 +5,10 @@ docker pull postgres:15.1
 docker pull jupyter/pyspark-notebook:spark-3.1.2
 
 echo "Building application image"
-docker build -f ../Dockerfile -t jupyter-app:v1.0 .
+docker build -f Dockerfile -t jupyter-app:v1.0 .
 
 echo "Building the cluster"
-docker-compose -f ../docker-compose.yml up -d
+docker-compose -f docker-compose.yml up -d
 
 echo "Enjoy!"
 echo "Open the browser and go to localhost:8888, insert password 'jovyan' and run the notebook"
